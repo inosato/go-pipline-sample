fecb9f71dd938cbe9ecd88b808596d8b31c66068
inosato <si17_21@yahoo.co.jp>
2020-03-29 01:16:21 +0900

    config with yaml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/inosato/go-pipline-sample
 
 go 1.14
+
+require github.com/go-yaml/yaml v2.1.0+incompatible
--- sprinkle/main.go
+++ sprinkle/main.go
@@ -3,32 +3,41 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/go-yaml/yaml"
 )
 
 const otherWord = "*"
 
-var transforms = []string{
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get" + otherWord,
-	"go" + otherWord,
-	"lets " + otherWord,
-}
+var transforms []string = []string{}
 
 func main() {
+	configTransform()
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		t := transforms[rand.Intn(len(transforms))]
 		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
 	}
+}
+
+func configTransform() {
+	var v struct {
+		Transforms []string
+	}
+	buf, err := ioutil.ReadFile("./transforms.yml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := yaml.Unmarshal(buf, &v); err != nil {
+		fmt.Println(err)
+		return
+	}
+	transforms = v.Transforms
 }
